f59fbf14b817e4a60e06a90bdece000fdb136d37
WMXPY <wm@mengw.io>
2021-06-01 23:11:17 -0500

    add simple hook

--- src/hook.ts
+++ src/hook.ts
@@ -3,3 +3,29 @@
  * @namespace InternationalizationReact
  * @description Hook
  */
+
+import { LOCALE, SudooFormat, SudooInternationalization } from "@sudoo/internationalization";
+import * as React from "react";
+import { InternationalizationContext, InternationalizationContextValue } from "./context";
+
+export const useInternationalization = (): InternationalizationContextValue => {
+
+    const context: InternationalizationContextValue = React.useContext(InternationalizationContext);
+
+    return context;
+};
+
+export const useLocale = (): LOCALE => {
+
+    const context: InternationalizationContextValue = useInternationalization();
+
+    return context.locale;
+};
+
+export const useFormat = (internationalization: SudooInternationalization): SudooFormat => {
+
+    const locale: LOCALE = useLocale();
+    const format: SudooFormat = internationalization.format(locale);
+
+    return format;
+};
