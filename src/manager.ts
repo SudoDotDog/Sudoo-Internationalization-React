a41ca8d79dc49591d6a83ac7e9d3e2852cb55179
WMXPY <wm@mengw.io>
2021-06-01 22:22:23 -0500

    implement mangaer

---
+++ src/manager.ts
@@ -0,0 +1,56 @@
+/**
+ * @author WMXPY
+ * @namespace InternationalizationReact
+ * @description Manager
+ */
+
+import { LOCALE } from "@sudoo/internationalization";
+
+export type InternationalizationListener = (locale: LOCALE) => void;
+
+export class InternationalizationManager {
+
+    private static _instance: InternationalizationManager | null = null;
+
+    public static getInstance(): InternationalizationManager {
+
+        if (this._instance) {
+            return this._instance;
+        }
+
+        this._instance = new InternationalizationManager();
+        return this._instance;
+    }
+
+    private _listeners: Map<string, InternationalizationListener>;
+
+    private constructor() {
+
+        this._listeners = new Map();
+    }
+
+    public get size(): number {
+
+        return this._listeners.size;
+    }
+
+    public addListener(key: string, listener: InternationalizationListener): this {
+
+        this._listeners.set(key, listener);
+        return this;
+    }
+
+    public removeListener(key: string): this {
+
+        this._listeners.delete(key);
+        return this;
+    }
+
+    public trigger(locale: LOCALE): this {
+
+        for (const listener of this._listeners.values()) {
+            listener(locale);
+        }
+        return this;
+    }
+}
