8b4536173714f7058a0c68e7a2e01f04ad3daceb
WMXPY <wm@mengw.io>
2021-06-03 17:03:11 -0500

    add with internationlization

--- src/consumer.ts
+++ src/consumer.ts
@@ -4,13 +4,14 @@
  * @description Consumer
  */
 
-import { LOCALE } from "@sudoo/internationalization";
+import { LOCALE, SudooFormat, SudooInternationalization } from "@sudoo/internationalization";
 import * as React from "react";
 import { InternationalizationContext, InternationalizationContextValue } from "./context";
 
 export type WithLocaleProps = {
 
     readonly locale: LOCALE;
+    readonly setLocale: (newLocale: LOCALE) => void;
 };
 
 export const withLocale = <P>(Component: React.ComponentType<P>): React.FC<P & WithLocaleProps> => {
@@ -27,6 +28,35 @@ export const withLocale = <P>(Component: React.ComponentType<P>): React.FC<P & W
                     ...originalProps,
                     locale: value.locale,
                     setLocale: value.setLocale,
+                });
+            },
+        );
+    };
+};
+
+export type WithInternationalizationProps = WithLocaleProps & {
+
+    readonly format: SudooFormat;
+};
+
+export const withInternationalization = <P>(
+    Component: React.ComponentType<P>,
+    internationalization: SudooInternationalization,
+): React.FC<P & WithLocaleProps> => {
+
+    return (originalProps: any) => {
+
+        return React.createElement(
+            InternationalizationContext.Consumer,
+            undefined,
+            (value: InternationalizationContextValue) => {
+
+                return React.createElement(Component, {
+
+                    ...originalProps,
+                    locale: value.locale,
+                    setLocale: value.setLocale,
+                    format: internationalization.format(value.locale),
                 });
             },
         );
