ad26a1470468aa5f3a818df5effced46937e64da
WMXPY <wm@mengw.io>
2021-06-17 17:09:06 -0500

    fix import

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@
  * @description Index
  */
 
+export * from "./builder/format";
+export * from "./builder/locale";
 export * from "./consumer";
 export * from "./context";
+export * from "./declare";
 export * from "./hook";
 export * from "./manager";
 export * from "./provider";
